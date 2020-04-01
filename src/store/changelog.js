bbe744b5790d2648fed675ebd16cdfd075026069
Tom Lynn <tom@lynn.id.au>
2020-04-01 20:41:00 +1000

    Updated changelog

--- src/store/changelog.js
+++ src/store/changelog.js
@@ -1,6 +1,7 @@
 export default [
+  'v0.0.005-alpha - made sizing changes for various resolutions, added vertical scrolling back. also cleared fuel lock if redeemed before expiry',
   'v0.0.004-alpha - logging in functionality and locking in a fuel lock for a specific fuel type',
   'v0.0.003-alpha - this changelog',
   'v0.0.002-alpha - environment tweaks to allow for development & production instances',
-  'v0.0.001-alpha - app creation'
+  'v0.0.001-alpha - hello world'
 ];
